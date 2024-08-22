b056b1081dc83867e9517772436f822b0bb82ce2
Guilherme Lessa <160809888+offlessa@users.noreply.github.com>
2024-08-22 16:30:43 -0300

    Inserir dado SQL

---
+++ inserir_dado.sql
@@ -0,0 +1,4 @@
+INSERT INTO Clientes (nome, email, data_nascimento)
+VALUES ('offlessa', 'off.lessa@gmail.com', '1990-05-15'),
+       ('Carlos Souza', 'carlos.souza@gmail.com', '1985-11-23'),
+       ('Maria Oliveira', 'maria.oliveira@gmail.com', '1992-08-07');
